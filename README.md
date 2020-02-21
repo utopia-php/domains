8019e8dc41a32618689f23a119fa38d811e712c4
Eldad Fux <eldad.fux@gmail.com>
2020-02-21 17:32:25 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# domains
-Lite &amp; fast micro PHP domain parsing library that is **easy to use**.
+# Utopia Domains
+
+** Work in progress!**
+
+[![Build Status](https://travis-ci.org/utopia-php/domains.svg?branch=master)](https://travis-ci.org/utopia-php/domains)
+![Total Downloads](https://img.shields.io/packagist/dt/utopia-php/domains.svg)
+[![Chat With Us](https://img.shields.io/gitter/room/utopia-php/community.svg)](https://gitter.im/utopia-php/community?utm_source=share-link&utm_medium=link&utm_campaign=share-link)
+
+Utopia Domains library is simple and lite library for parsing domain name structure. This library is aiming to be as simple and easy to learn and use.
+
+Although this library is part of the [Utopia Framework](https://github.com/utopia-php/framework) project it is **dependency free** and can be used as standalone with any other PHP project or framework.
+
+## Getting Started
+
+Install using composer:
+```bash
+composer require utopia-php/domains
+```
+
+```php
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Utopia\Domains\Domain;
+
+$domain = new Domain('demo.example.co.uk');
+
+$domain->get(); // demo.example.co.uk
+$domain->getTLD(); // .uk
+$domain->getSuffix(); // co.uk
+$domain->getName(); // example
+$domain->getSub(); // demo
+$domain->isKnown(); // true
+$domain->isICANN(); // true
+$domain->isPrivate(); // false
+$domain->isTest(); // false
+
+```
+
+## System Requirements
+
+Utopia Framework requires PHP 7.1 or later. We recommend using the latest PHP version whenever possible.
+
+## Authors
+
+**Eldad Fux**
+
++ [https://twitter.com/eldadfux](https://twitter.com/eldadfux)
++ [https://github.com/eldadfux](https://github.com/eldadfux)
+
+## Copyright and license
+
+The MIT License (MIT) [http://www.opensource.org/licenses/mit-license.php](http://www.opensource.org/licenses/mit-license.php)
---
+++ tests/DomainTest.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Utopia PHP Framework
+ *
+ * @package Domains
+ * @subpackage Tests
+ *
+ * @link https://github.com/utopia-php/framework
+ * @author Eldad Fux <eldad@appwrite.io>
+ * @version 1.0 RC4
+ * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+namespace Utopia\Tests;
+
+use Utopia\Domains\Domain;
+use PHPUnit\Framework\TestCase;
+
+class DomainTest extends TestCase
+{
+    /**
+     * @var Test
+     */
+    protected $test = null;
+
+    public function testExampleCoUk()
+    {
+        $domain = new Domain('demo.example.co.uk');
+       
+        $this->assertEquals('demo.example.co.uk', $domain->get());
+        $this->assertEquals('uk', $domain->getTLD());
+        $this->assertEquals('co.uk', $domain->getSuffix());
+        $this->assertEquals('example', $domain->getName());
+        $this->assertEquals('demo', $domain->getSub());
+        $this->assertEquals(true, $domain->isKnown());
+        $this->assertEquals(true, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(false, $domain->isTest());
+    }
+
+    public function testSubSubExampleCoUk()
+    {
+        $domain = new Domain('subsub.demo.example.co.uk');
+       
+        $this->assertEquals('subsub.demo.example.co.uk', $domain->get());
+        $this->assertEquals('uk', $domain->getTLD());
+        $this->assertEquals('co.uk', $domain->getSuffix());
+        $this->assertEquals('example', $domain->getName());
+        $this->assertEquals('subsub.demo', $domain->getSub());
+        $this->assertEquals(true, $domain->isKnown());
+        $this->assertEquals(true, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(false, $domain->isTest());
+    }
+
+    public function testLocalhost()
+    {
+        $domain = new Domain('localhost');
+       
+        $this->assertEquals('localhost', $domain->get());
+        $this->assertEquals('localhost', $domain->getTLD());
+        $this->assertEquals('', $domain->getSuffix());
+        $this->assertEquals('', $domain->getName());
+        $this->assertEquals('', $domain->getSub());
+        $this->assertEquals(false, $domain->isKnown());
+        $this->assertEquals(false, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(true, $domain->isTest());
+    }
+
+    public function testDemoLocalhost()
+    {
+        $domain = new Domain('demo.localhost');
+       
+        $this->assertEquals('demo.localhost', $domain->get());
+        $this->assertEquals('localhost', $domain->getTLD());
+        $this->assertEquals('', $domain->getSuffix());
+        $this->assertEquals('demo', $domain->getName());
+        $this->assertEquals('', $domain->getSub());
+        $this->assertEquals(false, $domain->isKnown());
+        $this->assertEquals(false, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(true, $domain->isTest());
+    }
+
+    public function testSubSubDemoLocalhost()
+    {
+        $domain = new Domain('sub.sub.demo.localhost');
+       
+        $this->assertEquals('sub.sub.demo.localhost', $domain->get());
+        $this->assertEquals('localhost', $domain->getTLD());
+        $this->assertEquals('', $domain->getSuffix());
+        $this->assertEquals('demo', $domain->getName());
+        $this->assertEquals('sub.sub', $domain->getSub());
+        $this->assertEquals(false, $domain->isKnown());
+        $this->assertEquals(false, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(true, $domain->isTest());
+    }
+
+    public function testSubDemoLocalhost()
+    {
+        $domain = new Domain('sub.demo.localhost');
+       
+        $this->assertEquals('sub.demo.localhost', $domain->get());
+        $this->assertEquals('localhost', $domain->getTLD());
+        $this->assertEquals('', $domain->getSuffix());
+        $this->assertEquals('demo', $domain->getName());
+        $this->assertEquals('sub', $domain->getSub());
+        $this->assertEquals(false, $domain->isKnown());
+        $this->assertEquals(false, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(true, $domain->isTest());
+    }
+
+    public function testUTF()
+    {
+        $domain = new Domain('אשקלון.קום');
+       
+        $this->assertEquals('אשקלון.קום', $domain->get());
+        $this->assertEquals('קום', $domain->getTLD());
+        $this->assertEquals('קום', $domain->getSuffix());
+        $this->assertEquals('אשקלון', $domain->getName());
+        $this->assertEquals('', $domain->getSub());
+        $this->assertEquals(true, $domain->isKnown());
+        $this->assertEquals(true, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(false, $domain->isTest());
+    }
+
+    public function testUTFSubdomain()
+    {
+        $domain = new Domain('חדשות.אשקלון.קום');
+       
+        $this->assertEquals('חדשות.אשקלון.קום', $domain->get());
+        $this->assertEquals('קום', $domain->getTLD());
+        $this->assertEquals('קום', $domain->getSuffix());
+        $this->assertEquals('אשקלון', $domain->getName());
+        $this->assertEquals('חדשות', $domain->getSub());
+        $this->assertEquals(true, $domain->isKnown());
+        $this->assertEquals(true, $domain->isICANN());
+        $this->assertEquals(false, $domain->isPrivate());
+        $this->assertEquals(false, $domain->isTest());
+    }
+}
